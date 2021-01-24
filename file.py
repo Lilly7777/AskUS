17de73b29fc2a8767dfb482f295ae30a4127fa59
Lilly Lubenova <lilly.lubenova@gmail.com>
2021-01-24 19:30:52 +0200

    Change file.py

--- file.py
+++ file.py
@@ -1,19 +1,61 @@
-from flask import Flask 
-app = Flask(__name__)
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import flash
+from hashlib import sha256
+from flask import redirect
+from flask_sqlalchemy import SQLAlchemy as sql
 
-@app.route("/")
-@app.route("/home")
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///askus.db" #TODO Ignore
+
+database = sql(app)
+
+class Member(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    uname = database.Column(database.String, nullable = False)
+    email = database.Column(database.String, nullable = False)
+    passwd = database.Column(database.String, nullable = False)
+
+    @staticmethod
+    def hash(passwd):
+        return sha256(passwd.encode('utf-8')).hexdigest()
+
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return "<h1>Home Page</h1>"
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        pass 
+        # TODO
 
 
 @app.route("/login")
 def login():
-    return "<h1>Login Page</h1>"
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        uname = request.form['inputUname']
+        passwd = request.form['inputPasswd']
+        #TODO token
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return "<h1>Register Page</h1>"
+    if request.method == 'GET':
+        return render_template('register.html')
+    if request.method == 'POST':
+        uname = request.form['inputUname']
+        email = request.form['inputEmail']
+        passwd = request.form['inputPasswd']
+        conf_passwd = request.form['inputConf_Passwd']
+
+        if passwd == conf_passwd:
+            database.session.add(Member(uname=uname, email = email, passwd = Member.hash(passwd)))
+            database.session.commit()
+            return redirect("/")
+        else:
+            flash("Password does not match")
+        #TODO token
 
 if __name__ == '__main__':
     app.run(debug=True)
